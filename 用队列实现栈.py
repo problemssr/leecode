f92d64969db0653b7a3da08ee083cc3c0ace4f8e
problemssr <2298269347@qq.com>
2022-11-12 23:15:15 +0800

    队列实现栈

---
+++ 用队列实现栈.py
@@ -0,0 +1,22 @@
+class MyStack:
+
+    def __init__(self):
+        self.que = deque()
+
+    def push(self, x: int) -> None:
+        self.que.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+        for i in range(len(self.que)-1):
+            self.que.append(self.que.popleft())
+        return self.que.popleft()
+
+    def top(self) -> int:
+        if self.empty():
+            return None
+        return self.que[-1]
+
+    def empty(self) -> bool:
+        return not self.que
