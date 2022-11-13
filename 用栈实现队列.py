90d87ced1ce952c009c0cc77f34fd68f5bdb02bd
problemssr <2298269347@qq.com>
2022-11-13 23:27:00 +0800

    栈实现队列

--- 用栈实现队列.py
+++ 用栈实现队列.py
@@ -1,18 +1,26 @@
 class MyQueue:
 
     def __init__(self):
+        """
+        in主要负责push，out主要负责pop
+        """
         self.stack_in = []
         self.stack_out = []
 
+
     def push(self, x: int) -> None:
-        # 有新元素进来，就往in里面push
+        """
+        有新元素进来，就往in里面push
+        """
         self.stack_in.append(x)
 
+
     def pop(self) -> int:
-        # 从队列前面移除元素并返回该元素
+        """
+        从队列前面移除元素并返回该元素
+        """
         if self.empty():
             return None
-
         if self.stack_out:
             return self.stack_out.pop()
         else:
@@ -21,20 +29,23 @@ class MyQueue:
             return self.stack_out.pop()
 
     def peek(self) -> int:
-        # 获取前面的元素
-        ans = self.pop()
+        """
+        返回队列开头的元素
+        """
+        ans=self.pop()
         self.stack_out.append(ans)
         return ans
 
+
     def empty(self) -> bool:
-        # 只要in或者out有元素，说明队列不为空
+        """只要in或者out有元素，说明队列不为空"""
         return not (self.stack_in or self.stack_out)
 
 
+
 # Your MyQueue object will be instantiated and called as such:
-x = 1
-obj = MyQueue()
-obj.push(x)
-param_2 = obj.pop()
-param_3 = obj.peek()
-param_4 = obj.empty()
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
